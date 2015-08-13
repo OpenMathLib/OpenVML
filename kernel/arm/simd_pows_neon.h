4b477f7cde91faf2fe76d2bb39d24c6ba682bb56
Yan Li <liyan@gmail.com>
2015-08-13 20:59:56 +0800

    Add the boundary check for exp function.

--- kernel/arm/simd_pows_neon.h
+++ kernel/arm/simd_pows_neon.h
@@ -8,7 +8,7 @@
 
 _PI32_CONST128(pow_uint_1, 1);
 _PI32_CONST128(pow_uint_0, 0);
-_PS128_CONST_TYPE(pow_nan, int, 0xffffffff);
+_PS128_CONST_TYPE(pow_nan, int, 0x7fffffff);
 
 static inline v4sf simd_pow4f(const v4sf aa, const v4sf bb)
 {
@@ -26,38 +26,36 @@ static inline v4sf simd_pow4f(const v4sf aa, const v4sf bb)
     const v4si uint_1 = *(v4si *) _pi32_128_pow_uint_1;
     const v4si uint_0 = *(v4si *) _pi32_128_pow_uint_0;
     v4si exp;
-	v4sf cc;
+    v4sf cc;
 
     {
         sign = simd_ands(mask, aa);
         abs_a = simd_fabs(aa);    //|a|
         abs_b = simd_fabs(bb);    //|b|
 
-        // using ln fuction 
+        /* using ln fuction */ 
         tem0 = simd_ln4f(abs_a);
         tem1 = simd_muls(tem0, bb);
 
-        // using exp fuction 
-		cc = simd_exp4f(tem1);
+        /* using exp fuction */ 
+        cc = simd_exp4f(tem1);
 
-#if 1
         exp = simd_cvts_w(bb);
 
         man = simd_castu_s(simd_cmpeqw(simd_andw(exp, uint_1), uint_0));   //even or odd
 
-        odd = simd_ors(sign, cc);      // or sign
+        odd = simd_ors(sign, cc);
         out1 = simd_adds(simd_ands(man, cc), simd_andnots(man, odd));
 
         floor_b = simd_floors(bb);
 
-        // x<0 and y != N, then NAN
+        /* x<0 and y != N, then NAN */
         man = simd_andnots(simd_cmpeqs(bb, floor_b), simd_cmplts(aa, zero));
         out2 = simd_adds(simd_ands(man, nan), simd_andnots(man, out1));
 
-        //y = 0 then 1
+        /* y = 0 then 1 */
         man = simd_cmpeqs(abs_b, zero);
         cc = simd_adds(simd_ands(man, sone), simd_andnots(man, out2));
-#endif
         return cc;
     }
 }
