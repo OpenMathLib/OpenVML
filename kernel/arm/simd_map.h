0422b8702aeb12460b9c8cc63de873035bacc773
Yan Li <liyan@gmail.com>
2015-08-13 20:59:56 +0800

    Add the boundary check for exp function.

--- kernel/arm/simd_map.h
+++ kernel/arm/simd_map.h
@@ -3,19 +3,19 @@
 
 #include "simd_const.h"
 
-#define simd_zero				vmovq_n_f32(0.0f) 	
+#define simd_zero			vmovq_n_f32(0.0f) 	
 
-#define simd_adds				vaddq_f32
-#define simd_addw				vaddq_s32	
-#define simd_andw				vandq_s32
-#define simd_notw				vmvnq_s32
-#define simd_subs				vsubq_f32
-#define simd_subw				vsubq_s32
-#define simd_muls				vmulq_f32
+#define simd_adds			vaddq_f32
+#define simd_addw			vaddq_s32	
+#define simd_andw			vandq_s32
+#define simd_notw			vmvnq_s32
+#define simd_subs			vsubq_f32
+#define simd_subw			vsubq_s32
+#define simd_muls			vmulq_f32
 #define simd_divs(a,b)			vmulq_f32(a,vrecpeq_f32(b)) 	
 #define simd_rcps(b)			vrecpeq_f32(b)
 
-#define simd_sqrts				vsqrtq_f32
+#define simd_sqrts			vsqrtq_f32
 #define simd_casts_w			vreinterpretq_s32_f32 // cast float to int
 #define simd_castw_s			vreinterpretq_f32_s32 // cast int to float
 #define simd_castu_s			vreinterpretq_f32_u32 // cast uint to float
@@ -42,8 +42,8 @@
 #define simd_sllw(a,b)			vshlq_n_s32(a,b) 
 #define simd_srlw(a,b)			vshrq_n_s32(a,b)
 
-#define simd_negs				vnegq_f32 // ri = -ai
-#define simd_fabs				vabsq_f32 // |va|
+#define simd_negs			vnegq_f32 // ri = -ai
+#define simd_fabs			vabsq_f32 // |va|
 
 // if a is true and select c, else b
 static inline v4sf simd_selects(v4sf a, v4sf b, v4sf c)
@@ -58,14 +58,14 @@ static inline v4sf simd_selects(v4sf a, v4sf b, v4sf c)
 #define simd_mas(a,b,c)			vaddq_f32(vmulq_f32(a,b),c) // vfmaq_f32(c,a,b) // vmlaq_f32(c,a,b) // va*vb+vc
 #define simd_nmas(a,b,c)		vsubq_f32(c,vmulq_f32(a,b)) // vfmsq_f32(c,a,b) // vmlsq_f32(c,a,b) // vc-va*vb
 
-#define simd_cvts_w				vcvtq_s32_f32 // convert float to int
-#define simd_cvtw_s				vcvtq_f32_s32 // convert int to float
-#define simd_cvtu_s				vcvtq_f32_u32 // convert uint to float
+#define simd_cvts_w			vcvtq_s32_f32 // convert float to int
+#define simd_cvtw_s			vcvtq_f32_s32 // convert int to float
+#define simd_cvtu_s			vcvtq_f32_u32 // convert uint to float
 
-#define simd_maxw				vmaxq_s32
-#define simd_minw				vminq_s32
+#define simd_maxw			vmaxq_s32
+#define simd_minw			vminq_s32
 
-#define simd_maxs				vmaxq_f32
-#define simd_mins				vminq_f32
+#define simd_maxs			vmaxq_f32
+#define simd_mins			vminq_f32
 
 #endif /*__SIMD_MAP_H */
