f01323cadaeac3c9fabf8a60fcad6130b325b056
Zhang Xianyi <traits.zhang@gmail.com>
2015-08-14 20:56:27 +0000

    Enable floor neon version for ARM Cortex-A15.

--- kernel/arm/sfloor_kernel_neon.c
+++ kernel/arm/sfloor_kernel_neon.c
@@ -1,24 +1,22 @@
-#include "openvml_function.h"
-#include "openvml_map.h"
 #include <math.h>
-//#include <omp.h>
+#include "openvml_kernel.h"
+#include "simd_map.h"
 
-void vs_floor(const unsigned int n, const float *a, float *b)
+void KERNEL_NAME(VMLLONG n, VML_FLOAT * a, VML_FLOAT * b, VML_FLOAT * y, VML_FLOAT * z, VML_FLOAT * other_params)
 {
     unsigned int m = n >> 2;
     unsigned int k = n & 3;
     unsigned int l = n & (~3);
 
     v4sf *src = (v4sf *) a;
-    v4sf *dest = (v4sf *) b;
+    v4sf *dest = (v4sf *) y;
 
-    //    #pragma omp parallel for
     for (unsigned int j = 0; j < m; j++) {
         v4sf tem = simd_floors(src[j]);
         dest[j] = tem;
     }
 
     for (unsigned int j = l; j < k; j++) {
-        b[j + l] = floorf(a[j + l]);
+        y[j + l] = floorf(a[j + l]);
     }
 }
