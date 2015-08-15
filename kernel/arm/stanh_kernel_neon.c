a4c8bb43bb27a1b1843d592b04c1caf201d6af9b
Yan Li <liyan@gmail.com>
2015-08-15 21:44:10 +0800

    Using relative error for accurary testing and fix bugs in neon kernels.

--- kernel/arm/stanh_kernel_neon.c
+++ kernel/arm/stanh_kernel_neon.c
@@ -9,16 +9,14 @@ void KERNEL_NAME(VMLLONG n, VML_FLOAT * a, VML_FLOAT * b, VML_FLOAT * y, VML_FLO
     unsigned int k = n & 3;
     unsigned int l = n & (~3);
 
-    v4sf *src = (v4sf *) a;
-    v4sf *dst = (v4sf *) y;
-
     //    #pragma omp parallel for
     for (unsigned int j = 0; j < m; j++) {
-        v4sf tem = simd_tanh4f(src[j]);
-        dst[j] = tem;
+        v4sf src = vld1q_f32(a + j * 4); 
+		v4sf tem = simd_tanh4f(src);
+		vst1q_f32(y + j * 4, tem); 
     }
 
-    for (unsigned int j = l; j < k; j++) {
+    for (unsigned int j = 0; j < k; j++) {
         y[j + l] = tanhf(a[j + l]);
     }
 }
